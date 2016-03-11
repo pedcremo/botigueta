a73405aaaa7ab7fb314fa12b86f091f2939ef588
nicklaus_ <pedcremo@gmail.com>
2016-03-11 22:09:50 +0100

    Change region EU

--- backend/models.js
+++ backend/models.js
@@ -3,8 +3,9 @@ var _ = require('underscore');
 
 module.exports = function(wagner) {
   //mongoose.connect('mongodb://localhost:27017/bocairent');
-  mongoose.connect('mongodb://pedcremo:pedcremo@ds017258.mlab.com:17258/heroku_1lps40vz');
-
+  //mongoose.connect('mongodb://pedcremo:pedcremo@ds017258.mlab.com:17258/heroku_1lps40vz');
+  mongoose.connect('mongodb://heroku_q6n2nng7:a344veou5fmvjhd37pupqo59d2@ds013619.mlab.com:13619/heroku_q6n2nng7');
+  //mongodb://heroku_q6n2nng7:a344veou5fmvjhd37pupqo59d2@ds013619.mlab.com:13619/heroku_q6n2nng7
   wagner.factory('db', function() {
     return mongoose;
   });
